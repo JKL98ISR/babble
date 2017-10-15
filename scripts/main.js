5c7303c53c4625f31a0544e0ac2bb773454283a7
JKL98ISR <jonatan.lib@gmail.com>
2017-10-15 18:48:13 +0300

    ch5

--- scripts/main.js
+++ scripts/main.js
@@ -109,7 +109,7 @@ function printMsg(msg){
     msgL.appendChild(img);
     var topBar=document.createElement("div"); 
     topBar.className="msgText";
-    topBar.tabIndex="0";
+    topBar.tabIndex="1";
     var userName=document.createElement("cite");
     userName.innerHTML=msg.name;
     topBar.appendChild(userName);
